d9f5bfdef0076d9895f9d4ab8cc8f0d9a4957936
Hellhound <hardoneloves@gmail.com>
2021-11-25 21:32:11 +0300

    Добавьте файлы проекта.

---
+++ 4__dz/4__dz.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+int main(){
+	long long int a[5], n, j = 0;
+	std::cin >> n;
+	for (int i = 0; i < 5; i++) {
+		std::cin >> a[i];
+	}
+	if (n > 0) {
+		for (int i = 0; i < n; i++) {
+			for (int i = 0; i < 4; i++) {
+				j = a[i];
+				a[i] = a[i + 1];
+				a[i + 1] = j;
+			}
+		}
+	}
+	else {
+		n = n * (-1);
+		for (int i = n; i > 0; i--) {
+			for (int i = 4; i > 0; i--) {
+				j = a[i];
+				a[i] = a[i - 1];
+				a[i - 1] = j;
+				std::cout << j << " ";
+			}
+		}
+		std::cout << std::endl;
+	}
+	for (int i = 0; i < 5; i++) {
+		std::cout << a[i] << " ";
+	}
+}
